03b909ebba692dd273cd3f9c5fce59678240538c
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-09-12 14:53:12 -0300

    Criado o list-controller.js

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,5 +1,6 @@
 import Address from "../models/address.js";
 import * as addressService from "../services/address-service.js";
+import * as listController from "./list-controller.js";
 
 function State() {
   this.address = new Address();
@@ -70,7 +71,7 @@ function handleBtnClearClick(event) {
 }
 async function handleBtnSaveClick(event) {
   event.preventDefault();
-  console.log(state.address);
+  listController.addCard(state.address);
 }
 function clearForm() {
   state.inputCep.value = "";
